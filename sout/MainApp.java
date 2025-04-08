b6b428fc236cf96937594c817b2b17a2e71b4a13
justmedanel <daniel.ursu3@student.upt.ro>
2025-04-08 22:23:25 +0300

    added java program

---
+++ sout/MainApp.java
@@ -0,0 +1,11 @@
+package sout;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+    }
+}
