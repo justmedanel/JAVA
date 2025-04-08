6a1fa9ce45eca8e7675bf97de1353e3635e1ef63
justmedanel <daniel.ursu3@student.upt.ro>
2025-04-09 00:21:08 +0300

    divizori si daca nr este prim

--- sout/MainApp.java
+++ sout/MainApp.java
@@ -1,38 +1,37 @@
 package sout;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintStream;
-import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class MainApp {
     public static void main(String[] args) {
-        int suma = 0;
-        int count = 0;
-        int min = 99999999;
-        int max = 0;
-    try(Scanner scanner = new Scanner(new File("in.txt"))) {
-        while(scanner.hasNextInt()) {
-            int x = scanner.nextInt();
-            System.out.println(x);
-            suma += x;
-            count++;
-            if(x < min) min = x;
-            if (x > max) max = x;
+        // Citirea unui număr natural de la tastatură
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduceți un număr natural: ");
+        int n = scanner.nextInt();
 
+        // Verificarea dacă numărul este prim și găsirea divizorilor
+        if (n <= 1) {
+            System.out.println("Numărul trebuie să fie mai mare decât 1.");
+        } else {
+            boolean isPrime = true;  // Presupunem că numărul este prim
+            System.out.print("Divizorii lui " + n + " sunt: ");
 
+            for (int i = 1; i <= n; i++) {
+                if (n % i == 0) {
+                    System.out.print(i + " ");
+                    if (i != 1 && i != n) {
+                        isPrime = false;  // Dacă găsim un divizor diferit de 1 și n, numărul nu este prim
+                    }
+                }
+            }
+
+            // Dacă numărul este prim, afișăm un mesaj corespunzător
+            if (isPrime) {
+                System.out.println("\nNumărul este prim.");
+            } else {
+                System.out.println("\nNu e nr prim");  // Linia goală pentru separare
+            }
         }
-    }catch(FileNotFoundException e){
-        System.out.println("fisierul nu a fost gasit");
-        }
-    try(PrintWriter writer = new PrintWriter(new File("out.txt"))) {
-        writer.println("Suma= " + suma);
-        writer.println("Media aritmetica= " + (suma / (double) count));
-        writer.println("Min= " + min);
-        writer.println("Max= " + max);
-    }catch(FileNotFoundException e){
-        System.out.println("fisierul nu a fost gasit");
-        }
+
+        scanner.close();  // Închidem scanner-ul
     }
 }
