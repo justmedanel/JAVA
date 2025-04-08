a30616ecf17676f4eb07e6a38fbcec54b0200f43
justmedanel <daniel.ursu3@student.upt.ro>
2025-04-08 22:33:26 +0300

    Perimetrul si aria unui dreptunghi

--- sout/MainApp.java
+++ sout/MainApp.java
@@ -1,11 +1,16 @@
 package sout;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+
 import java.util.Scanner;
 
 public class MainApp {
     public static void main(String[] args) {
-
+        int a;
+        int b;
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        System.out.println("Perimetrul: "+2*(a+b));
+        System.out.println("Aria: " +(a*b));
     }
 }
